771401be0b5d965a65c45cd3358ba41220a12a80
Piyush <Piyush31009@gmail.com>
2022-08-25 23:06:14 +0530

    voice assistance

---
+++ main.py
@@ -0,0 +1,40 @@
+
+from multiprocessing.connection import Listener
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import pywhatkit
+import subprocess
+
+listener=sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('Listenning...')
+            voice=listener.listen(source)
+            command=listener.recognize_google(voice)
+            command=command.lower()
+            if 'alexa' in command:
+                command=command.replace('alexa',' ') 
+                          
+                print(command)
+            
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command=take_command()
+    if 'play' in command:
+        talk('playing')
+        print("playing songs")
+
+run_alexa()
