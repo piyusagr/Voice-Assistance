11af0dc3f203e5519555ea474f51e6f6399e6a1a
Piyush <Piyush31009@gmail.com>
2022-08-27 19:49:44 +0530

    voiceassistant

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pyttsx3
 import datetime
 import pywhatkit
 import wikipedia
+import pyjokes
 import webbrowser
 
 listener=sr.Recognizer()
@@ -48,6 +49,35 @@ def run_alexa():
         sourc=wikipedia.summary(tell,1)
         talk(sourc)
         print(sourc)
-    
-    
-run_alexa()
+    elif 'date' in command:
+        talk('sorry, i have not free')
+    elif 'are you single' in command:
+        talk('no, i am not single')
+    elif 'joke'  in command:
+        joke=pyjokes.get_joke()
+        talk(joke)
+        print(joke)
+    elif 'open youtube' in command:
+            talk("Here you go to Youtube\n")
+            webbrowser.open("youtube.com")
+ 
+    elif 'open google' in command:
+            talk("Here you go to Google\n")
+            webbrowser.open("google.com")
+ 
+    # elif 'open stackoverflow' in command:
+    #         talk("Here you go to Stack Over flow.Happy coding")
+    #         webbrowser.open("stackoverflow.com")  
+    # elif 'open github' in command:
+    #        text=command.replace('open github of','')
+    #        talk("here you go to github of"+text)
+    #        webbrowser.open("github.com/"+text)
+    # elif 'browse'  in command:
+    #        text=command.replace('browse','')
+    #        talk('here you go with'+text)
+    #        webbrowser.open('google.com/'+text)
+    else:
+        talk('plz say the command again')
+        
+while True:
+    run_alexa()
